6fcfe6c94480a3c647f50dc656dcbace1c04b858
sungminna <sungmin.na330@gmail.com>
2021-05-14 15:34:34 +0900

    build both project

---
+++ WordChainGame/ClientSocket.cpp
@@ -0,0 +1,20 @@
+// ClientSocket.cpp : 구현 파일입니다.
+//
+
+#include "stdafx.h"
+#include "WordChainGame.h"
+#include "ClientSocket.h"
+
+
+// CClientSocket
+
+CClientSocket::CClientSocket()
+{
+}
+
+CClientSocket::~CClientSocket()
+{
+}
+
+
+// CClientSocket 멤버 함수
